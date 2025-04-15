bc3e52c60651d6343ea7cbcaf7c5af882eda4cde
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-15 20:54:10 -0300

    refactor: Remover a validação

---
+++ middleware/user.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    if (!token) return res.status(401).json({ message: "Acesso negado!" });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select("-senha");
+
+    if (!req.user) return res.status(401).json({ message: "Usuário não encontrado!" });
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token inválido!", error });
+  }
+};
+
+module.exports = { verifyToken };
