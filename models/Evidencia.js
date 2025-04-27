b15dec50b833336898b41686a933a0a80301c25e
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-04-27 19:57:50 -0300

    feat: adicionar export

--- models/Evidencia.js
+++ models/Evidencia.js
@@ -4,3 +4,5 @@ const Evidencia = new mongoose.Schema({
     titulo: { type: String, required: true },
     anexo: { type: String, required: true }
 })
+
+module.exports = mongoose.model("Evidencia", CasoSchema);
