84b0bbda4a70fd7ad0db00a31f35aa24ae27eaec
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-05-09 14:42:11 -0300

    build: criar model Evidencia

---
+++ models/Evidencia.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const evidenciaSchema = new mongoose.Schema({
+    casoId: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Caso', 
+        required: true 
+    },
+    arquivoId: { type: mongoose.Schema.Types.ObjectId, required: true }, // ID do arquivo no GridFS
+    nomeArquivo: { type: String, required: true }, // Nome original do arquivo
+    tipoArquivo: { type: String, required: true }, // MIME type (ex.: image/jpeg, application/pdf)
+    tipoEvidencia: { 
+        type: String, 
+        enum: [
+            'radiografia', 
+            'odontograma',
+            'outro'
+        ], 
+        required: true 
+    },
+    descricao: { type: String },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Evidencia', evidenciaSchema);
