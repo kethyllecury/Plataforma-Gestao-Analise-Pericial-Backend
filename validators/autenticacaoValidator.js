26d802f3018d82aeaf0ee4510cbc28deee4eca97
Gabriel de Santana <joaog.santana2006@gmail.com>
2025-05-12 10:17:07 -0300

    fix: mudar mensagem validarAdmin

--- validators/autenticacaoValidator.js
+++ validators/autenticacaoValidator.js
@@ -26,7 +26,7 @@ const validarLogin = [
 
 const validarAdmin = (req, res, next) => {
     if(req.user == undefined) {
-        return res.status(403).json({ message: "Faça o login para efetuar operação!"})
+        return res.status(403).json({ message: "Faça o login como administrador para realizar operação!"})
     }
 
     if (req.user.cargo !== "admin") {
